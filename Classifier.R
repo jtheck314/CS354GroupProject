35cc056dea6799c0445af6287c1f9d113699fbc2
MintyFresh22 <54689080+MintyFresh22@users.noreply.github.com>
2020-11-14 22:13:50 -0700

    Classier Working

--- Classifier.R
+++ Classifier.R
@@ -1,50 +1,161 @@
 #File:Classifier.R
 
 
-mean.stdv.frame <- as.data.frame(fromJSON(file = "MeanStdDevCR.json"))
+mean.stdv.CR <- as.data.frame(fromJSON(file = "MeanStdDevCR.json"))
 
-#Vector of Functions for normal distribution
-danceProb <- function(danceValue){
+mean.stdv.Country <- as.data.frame(fromJSON(file = "MeanStdDevCountry.json"))
+
+#All Prob functions find the likelihood of their parameter on the standard normal curve
+danceProb <- function(danceValue = 0.0){
   dnorm(x=danceValue,
-        mean=mean.stdv.frame$danceMean, 
-        sd=mean.stdv.frame$danceStdDev)
+        mean=0, 
+        sd=1)
 }
-energyProb <- function(energyValue){
+
+energyProb <- function(energyValue = 0.0){
   dnorm(x=danceValue,
-        mean=mean.stdv.frame$energyMean, 
-        sd=mean.stdv.frame$energyStdDev)
+        mean=0, 
+        sd=1)
 }
-loudnessProb <- function(loudnessValue){
+
+loudnessProb <- function(loudnessValue = 0.0){
   dnorm(x=loudnessValue,
-        mean=mean.stdv.frame$loudnessMean, 
-        sd=mean.stdv.frame$loudnessStdDev)
+        mean=0, 
+        sd=1)
 } 
-speechinessProb <- function(speechinessValue){
+
+speechinessProb <- function(speechinessValue = 0.0){
   dnorm(x=speechinessValue,
-        mean=mean.stdv.frame$speechinessMean, 
-        sd=mean.stdv.frame$speechinessStdDev)
+        mean=0, 
+        sd=1)
 } 
-acousticnessProb <- function(acousticnessValue){
+
+acousticnessProb <- function(acousticnessValue = 0.0){
   dnorm(x=acousticnessValue,
-        mean=mean.stdv.frame$acousticnessMean, 
-        sd=mean.stdv.frame$acousticnessStdDev)
+        mean=0, 
+        sd=1)
 } 
-livenessProb <- function(livenessValue){
+
+livenessProb <- function(livenessValue = 0.0){
   dnorm(x=livenessValue,
-        mean=mean.stdv.frame$livenessMean, 
-        sd=mean.stdv.frame$livenessStdDev)
+        mean=0, 
+        sd=1)
 } 
-valenceProb <- function(valenceValue){
+
+valenceProb <- function(valenceValue = 0.0){
   dnorm(x=valenceValue,
-        mean=mean.stdv.frame$valenceMean, 
-        sd=mean.stdv.frame$valenceStdDev)
+        mean=0, 
+        sd=1)
 }
-tempoProb <- function(tempoValue){
+
+tempoProb <- function(tempoValue = 0.0){
   dnorm(x=tempoValue,
-        mean=mean.stdv.frame$tempoMean, 
-        sd=mean.stdv.frame$tempoStdDev)
+        mean=0, 
+        sd=1)
 }
 
-seqr <- seq(from=0,to=500,len=100)
+#Standardize the attributes of a Classic Rock song to the standard normal distribution
+stdAttrbCR <- function(song = NULL){
+  if(is.null(song)){
+    return(NULL)
+  }
+  song$dance <- (song$dance-mean.stdv.CR$danceMean)/(mean.stdv.CR$danceStdDev)
+  song$energy <- (song$energy-mean.stdv.CR$energyMean)/(mean.stdv.CR$energyStdDev)
+  song$loudness <- (song$loudness-mean.stdv.CR$loudnessMean)/(mean.stdv.CR$loudnessStdDev)
+  song$speechiness <- (song$speechiness-mean.stdv.CR$speechinessMean)/(mean.stdv.CR$speechinessStdDev)
+  song$acousticness <- (song$acousticness-mean.stdv.CR$acousticnessMean)/(mean.stdv.CR$acousticnessStdDev)
+  song$liveness <- (song$liveness-mean.stdv.CR$livenessMean)/(mean.stdv.CR$livenessStdDev)
+  song$valence <- (song$valence-mean.stdv.CR$valenceMean)/(mean.stdv.CR$valenceStdDev)
+  song$tempo <- (song$tempo-mean.stdv.CR$tempoMean)/(mean.stdv.CR$tempoStdDev)
+  return(song)
+}
 
-plot(seqr,tempoProb(seqr))
+#Standardize the attributes of a Country song to the standard normal distribution
+stdAttrbCountry <- function(song = NULL){
+  if(is.null(song)){
+    return(NULL)
+  }
+  song$dance <- (song$dance-mean.stdv.Country$danceMean)/(mean.stdv.Country$danceStdDev)
+  song$energy <- (song$energy-mean.stdv.Country$energyMean)/(mean.stdv.Country$energyStdDev)
+  song$loudness <- (song$loudness-mean.stdv.Country$loudnessMean)/(mean.stdv.Country$loudnessStdDev)
+  song$speechiness <- (song$speechiness-mean.stdv.Country$speechinessMean)/(mean.stdv.Country$speechinessStdDev)
+  song$acousticness <- (song$acousticness-mean.stdv.Country$acousticnessMean)/(mean.stdv.Country$acousticnessStdDev)
+  song$liveness <- (song$liveness-mean.stdv.Country$livenessMean)/(mean.stdv.Country$livenessStdDev)
+  song$valence <- (song$valence-mean.stdv.Country$valenceMean)/(mean.stdv.Country$valenceStdDev)
+  song$tempo <- (song$tempo-mean.stdv.Country$tempoMean)/(mean.stdv.Country$tempoStdDev)
+  return(song)
+}
+
+#Returns the classifier value given the song is Classic Rock
+classifyCR <- function(CRPrior = NULL,song = NULL){
+  if(is.null(song) | is.null(CRPrior)){
+    return(0)
+  }
+  
+  classifyValue <- 0
+  song <- stdAttrbCR(song)
+  if(!(is.null(song))){
+    classifyValue <- log(CRPrior) +
+                    log(danceProb(song$dance)) +
+                    log(energyProb(song$energy)) +
+                    log(loudnessProb(song$loudness)) +
+                    log(speechinessProb(song$speechiness)) +
+                    log(acousticnessProb(song$acousticness)) +
+                    log(livenessProb(song$liveness)) +
+                    log(valenceProb(song$valence)) +
+                    log(tempoProb(song$tempo))
+  }
+  return(classifyValue)
+}
+
+#Returns the classifier value given the song is Country
+classifyCountry <- function(CountryPrior = NULL,song = NULL){
+  if(is.null(song) | is.null(CountryPrior)){
+    return(0)
+  }
+  
+  classifyValue <- 0
+  song <- stdAttrbCountry(song)
+  if(!(is.null(song))){
+    classifyValue <- log(CountryPrior) +
+                    log(danceProb(song$dance)) +
+                    log(energyProb(song$energy)) +
+                    log(loudnessProb(song$loudness)) +
+                    log(speechinessProb(song$speechiness)) +
+                    log(acousticnessProb(song$acousticness)) +
+                    log(livenessProb(song$liveness)) +
+                    log(valenceProb(song$valence)) +
+                    log(tempoProb(song$tempo))
+  }
+  return(classifyValue)
+}
+
+#Returns the classifier's determination
+classify <- function(CRPrior = NULL, CountryPrior, song = NULL){
+  if(is.null(CRPrior) | is.null(CountryPrior) | is.null(song)){
+    return(-1)
+  }
+  CR.result <- classifyCR(CRPrior = CRPrior, song = song)
+  Country.result <- classifyCountry(CountryPrior = CountryPrior, song = song)
+
+  selection.result <- NULL
+  if(CR.result > Country.result){
+    selection.result <- "Classic Rock"
+  }
+  else{
+    selection.result <- "Country"
+  }
+  return(selection.result)
+}
+
+#MONTE TESTING THE CLASSIFIER
+songCR <- list(dance = .46,energy = .77,loudness = -8.0,speechiness = .069,
+          acousticness = .137,liveness = .295,valence = .46,tempo = 122.0)
+
+songCountry <- list(dance = .588,energy = .699,loudness = -5.74,speechiness = .045,
+               acousticness = .23,liveness = .166,valence = .544,tempo = 126.88)
+
+print(classify(CRPrior = .5,CountryPrior = .5,song = songCR))
+cat("\n")
+print(classify(CRPrior = .5,CountryPrior = .5,song = songCountry))
+
--- DataCalculation.R
+++ DataCalculation.R
@@ -3,9 +3,11 @@
 # Created by: donww
 # Created on: 11/1/2020
 
-file <- "TestingCR"
+fileCR <- "TestingCR"
+fileCountry <- "TestingCountry"
 
-result <- rjson::fromJSON(file = paste(c(file, ".json"), collapse = ""))
+resultCR <- rjson::fromJSON(file = paste(c(fileCR, ".json"), collapse = ""))
+resultCountry <- rjson::fromJSON(file = paste(c(fileCountry, ".json"), collapse = ""))
 
 danceData <- c()
 energyData <- c()
@@ -16,9 +18,9 @@ livenessData <- c()
 valenceData <- c()
 tempoData <- c()
 
-len <- length(result)
+len <- length(resultCR)
 
-for(song in result){
+for(song in resultCR){
   print(song$name)
   danceData <- append(danceData, song$dance)
   energyData <- append(energyData, song$energy)
@@ -32,7 +34,7 @@ for(song in result){
 
 
 # Create the data frame.
-dataTable.data <- data.frame(
+dataTable.data.CR <- data.frame(
     danceMean = mean(danceData),
     danceStdDev = sd(danceData),
     energyMean = mean(energyData),
@@ -51,6 +53,54 @@ dataTable.data <- data.frame(
     tempoStdDev = sd(tempoData),
     stringsAsFactors = FALSE
 )
-#paste(c(file, "Data", ".json"), collapse = "")
+#paste(c(fileCR, "Data", ".json"), collapse = "")
 
-write(rjson::toJSON(dataTable.data, ), file = "MeanStdDevCR.json")
+write(rjson::toJSON(dataTable.data.CR, ), file = "MeanStdDevCR.json")
+
+#Create MeanStdDevCountry.json
+danceData <- c()
+energyData <- c()
+loudnessData <- c()
+speechinessData <- c()
+acousticnessData <- c()
+livenessData <- c()
+valenceData <- c()
+tempoData <- c()
+
+len <- length(resultCountry)
+
+for(song in resultCountry){
+  print(song$name)
+  danceData <- append(danceData, song$dance)
+  energyData <- append(energyData, song$energy)
+  loudnessData <- append(loudnessData, song$loudness)
+  speechinessData <- append(speechinessData, song$speechiness)
+  acousticnessData <- append(acousticnessData, song$acousticness)
+  livenessData <- append(livenessData, song$liveness)
+  valenceData <- append(valenceData, song$valence)
+  tempoData <- append(tempoData, song$tempo)
+}
+
+
+# Create the data frame.
+dataTable.data.Country <- data.frame(
+  danceMean = mean(danceData),
+  danceStdDev = sd(danceData),
+  energyMean = mean(energyData),
+  energyStdDev = sd(energyData),
+  loudnessMean = mean(loudnessData),
+  loudnessStdDev = sd(loudnessData),
+  speechinessMean = mean(speechinessData),
+  speechinessStdDev = sd(speechinessData),
+  acousticnessMean = mean(acousticnessData),
+  acousticnessStdDev = sd(acousticnessData),
+  livenessMean = mean(livenessData),
+  livenessStdDev = sd(livenessData),
+  valenceMean = mean(valenceData),
+  valenceStdDev = sd(valenceData),
+  tempoMean = mean(tempoData),
+  tempoStdDev = sd(tempoData),
+  stringsAsFactors = FALSE
+)
+
+write(rjson::toJSON(dataTable.data.Country, ), file = "MeanStdDevCountry.json")
