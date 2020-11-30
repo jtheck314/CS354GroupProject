c48bdaaafed2fc3507b1a5139984fa69ded96323
Donny <donvoowright@aol.com>
2020-11-29 18:51:15 -0700

    Add files via upload

---
+++ Runner/runner.R
@@ -0,0 +1,27 @@
+source(file="playlistDataGrabber.R")
+source(file="getSongData.R")
+source(file="Classifier.R")
+
+cat("Welcome to the Spotify Naive Bayes Classifier! What mode would you like to run? \n 1: Data Grabber \n 2: Song Genre Guesser\n 3: Quit.\n")
+user.mode <- readline("What mode would you like to ")
+if (user.mode == "1"){
+  cat("\n Please input a Spotify platylist id(example: 30gdZjDdQGUvBFe5XaT3XU): ")
+  user.playlist.id <- readline()
+  cat("\n How many songs are in this playlist? ")
+  user.playlist.len <- strtoi(readline())
+  cat("\n What should I call the file? ")
+  user.playlist.name <- readline()
+  compileData(user.playlist.id, user.playlist.len, user.playlist.name)
+}else if(user.mode == "2"){
+  cat("\n Please input a Spotify song id(example: 6DtmnN7wK6m8dI8UNaz3sG): ")
+  user.song.id <- readline()
+  songData <- getSongDataAndFormat(user.song.id)
+  result <- classify(CRPrior = (1/3),CountryPrior = (1/3),ClassicalPrior = (1/3), song = songData)
+  cat(paste("\nGuess: ", result))
+}else{
+  print("Quitting...")
+}
+
+
+
+
